1a9b2e7e7664391e66d696447a67112573114827
Matt Goff <mgoff72@gmail.com>
2020-01-18 13:35:18 -0700

    Complete N-Rooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,9 +16,37 @@
 
 
 window.findNRooksSolution = function (n) {
-  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(myBoard.rows()));
+  // find a single valid configuration of n rooks
+  var solution = undefined; //fixme
+  var newBoard = new Board({ n: n });
+  // new board of size n
+  // iterate over the board looking for conflicts
+
+  var looper = function (row) {
+    if (row === n) {
+      return newBoard.rows();
+    }
+    for (let col = 0; col < n; col++) {
+      // set the first piece in the top left corner
+      newBoard.togglePiece(row, col);
+      // then check for conflicts
+      // if there aren't any conflicts, then increment the row counter so as to move on
+      // if there ARE conflicts, then move the rook one position right and check again?
+
+      if (newBoard.hasAnyRooksConflicts() === false) {
+        // loop again but with row++ as the new row index
+        return looper(row + 1);
+      }
+      // if we make it here it's becuase there was a rook conflict
+      newBoard.togglePiece(row, col);
+    }
+  };
+
+
+  solution = looper(0);
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
@@ -26,12 +54,39 @@ window.findNRooksSolution = function (n) {
 window.countNRooksSolutions = function (n) {
   var solutionCount = 0;
 
+  var newBoard = new Board({ n: n });
+  // new board of size n
+  // iterate over the board looking for conflicts
+
+  var looper = function (row) {
+    if (row === n) {
+      solutionCount++;
+    } else {
+
+      for (let col = 0; col < n; col++) {
+        // set the first piece in the top left corner
+        newBoard.togglePiece(row, col);
+        // then check for conflicts
+        // if there aren't any conflicts, then increment the row counter so as to move on
+        // if there ARE conflicts, then move the rook one position right and check again?
+
+        if (newBoard.hasColConflictAt(col) === false) {
+          // loop again but with row++ as the new row index
+          looper(row + 1);
+        }
+        // if we make it here it's becuase there was a rook conflict
+        newBoard.togglePiece(row, col);
+      }
+    }
+  };
+  // debugger;
+  looper(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function (n) {
+window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
@@ -39,7 +94,7 @@ window.findNQueensSolution = function (n) {
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function (n) {
+window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
